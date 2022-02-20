2cd9848ffe16bd5561ab26684fb3ed5d959d12dd
Oshan Shrestha <os02411418@student.ku.edu.np>
2022-02-20 12:59:52 +0545

    router setup init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
-	return <div>Hello Purlieu Users</div>;
+	return (
+		<div>
+			<AppRoutes />
+		</div>
+	);
 };
 
 export default App;
